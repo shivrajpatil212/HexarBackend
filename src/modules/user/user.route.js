aef01246cc53056954fa6744615c2b2bf54a78d6
shivraj <shivraj.laxman.patil@techiebears.com>
2026-04-09 00:04:44 +0530

    applied auth Limiter for login

--- src/modules/user/user.route.js
+++ src/modules/user/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { login } from './user.controller.js';
 import { loginValidation } from './user.validation.js';
+import { authLimiter } from '../../config/ratelimit.js';
 
 const userRoute = Router();
 
-userRoute.post("/login", loginValidation, login);
+userRoute.post("/login", authLimiter, loginValidation, login);
 
 export default userRoute;
